7829aeab6bb45f9ca80e031991c76018bedb2b15
seiry <4397354+seiry@users.noreply.github.com>
2023-09-10 03:58:25 +0800

    buildImmortalwrt

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-sed -i 's#src-git packages https://github.com/Lienol/openwrt-packages.git;22.03#src-git packages https://github.com/seiry/openwrt-packages.git;22.03#' feeds.conf.default
+# sed -i 's#src-git packages https://github.com/Lienol/openwrt-packages.git;22.03#src-git packages https://github.com/seiry/openwrt-packages.git;22.03#' feeds.conf.default
